8809879c13dad2bd9e582364d26222d4a47cb36e
Tejas Singh <tejassingh649@gmail.com>
2022-12-23 02:13:02 +0530

    Initial Lineage-20

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Amritorock/local_manifest.git --depth 1 -b los .repo/local_manifests
+git clone https://github.com/baconpeedit/Local_Manifest.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
 source build/envsetup.sh
-lunch lineage_r5x-userdebug
-export TZ=Asia/Dhaka #put before last build command
+lunch lineage_ginkgo-userdebug
+export TZ=Asia/Delhi #put before last build command
 make bacon 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
