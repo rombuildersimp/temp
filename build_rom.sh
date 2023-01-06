37841d41a99d1c5a878d67c5f1f518c143a3b2f5
Suraj Kumar <surajkumar18jun2004@gmail.com>
2023-01-07 00:27:12 +0530

    EOL PIXEL EXPERIENCE

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manif
 git clone https://github.com/ImSurajx/manifest --depth 1 -b Pe-12L .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 . build/envsetup.sh
 lunch aosp_lavender-user
 export KBUILD_BUILD_USER=ImSurajx 
