7dc6271e99893974253d1bd9b96bfd6814502880
Dooms <vmbuildforme@gmail.com>
2023-01-06 22:48:58 +0530

    for testing

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android.git -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Dooms-v/local-manifest.git --depth 1 -b rice-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
